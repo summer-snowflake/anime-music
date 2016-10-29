942360e5d648b2d1f350eb01c0a2da705f7d0044
kae_kasui <kaekasui_github@icloud.com>
2016-10-29 20:12:30 +0900

    Add the requests api spec

---
+++ spec/requests/animes_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe 'GET /api/animes' do
+  let!(:anime1) { create(:anime) }
+  let!(:anime2) { create(:anime) }
+
+  it '201とアニメ一覧が返ってくること' do
+    get '/api/animes'
+    expect(response.status).to eq 200
+
+    json = { 
+      animes: [
+        {   
+          id: anime1.id,
+          title: anime1.title
+        },  
+        {   
+          id: anime2.id,
+          title: anime2.title
+        }   
+      ]   
+    }   
+    expect(response.body).to be_json_as(json)
+  end
+end
