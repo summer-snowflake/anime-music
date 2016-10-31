7cc382bc267e065497973c5cf1b8837c7dff803a
kae_kasui <kaekasui_github@icloud.com>
2016-10-31 23:56:41 +0900

    Modify following rubocop about that line is too long

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,10 @@ Capybara.default_max_wait_time = 20
 Capybara.register_driver :mobile do |app|
   Capybara::RackTest::Driver.new(
     app,
-    headers: { 'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53' }
+    headers: { 'HTTP_USER_AGENT' =>
+               'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) \
+               AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 \
+               Mobile/11A465 Safari/9537.53' }
   )
 end
 
