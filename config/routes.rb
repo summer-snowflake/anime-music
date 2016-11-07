ef67c08ac6281a329d4a1ed12ad71a428779b30c
kae_kasui <kaekasui_github@icloud.com>
2016-11-08 02:13:13 +0900

    Add the melody crud

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     root to: 'top#index', as: :top
     resources :animes
     resources :actors
+    resources :melodies
   end
 
   namespace :api, only: %w(index), format: 'json' do
