77fcbc35369312e92eda1d22a89dc9d0307737a6
kae_kasui <kaekasui_github@icloud.com>
2016-11-12 01:47:51 +0900

    Add eslint and eslint-plugin-react

--- app/assets/javascripts/__test__/anime_list.test.js
+++ app/assets/javascripts/__test__/anime_list.test.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AnimeList from './../components/anime_list.jsx';
-import { shallow } from 'enzyme';
-jest.unmock('./../components/anime_list.jsx');
+import React from 'react'
+import AnimeList from './../components/anime_list.jsx'
+import { shallow } from 'enzyme'
+jest.unmock('./../components/anime_list.jsx')
 
 describe('AnimeListComponent', () => {
   it('アニメ情報一覧が表示されること', () => {
-    const anime_list = shallow(<AnimeList url='api/animes'></AnimeList>);
+    const anime_list = shallow(<AnimeList url='api/animes' />)
 
     anime_list.setState(
       { animes: [{ id: 1, title: 'タイトル' }, { id: 2, title: 'タイトル2' }] }
-    );
+    )
     expect(anime_list.find('Anime').length).toEqual(2)
-  });
-});
+  })
+})
--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,10 +1,11 @@
 //= require_self
 //= require react_ujs
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-window.React = React;
-window.ReactDOM = ReactDOM;
+window.React = React
+window.ReactDOM = ReactDOM
 
-window.AnimeList = require('./components/anime_list.jsx');
+import AnimeList from './components/anime_list.jsx'
+window.AnimeList = AnimeList
