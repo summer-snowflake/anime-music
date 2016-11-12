146ee167d244369b350cb362a9fde920b8627a11
kae_kasui <kaekasui_github@icloud.com>
2016-11-12 19:34:21 +0900

    Modify jsx

--- app/assets/javascripts/components/anime.jsx
+++ app/assets/javascripts/components/anime.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-var Anime = React.createClass({
-  render: function() {
+class Anime extends React.Component {
+  render () {
     return (
       <div className='panel panel-default anime-title'>
         <div className='panel-body'>
           { this.props.children }
         </div>
       </div>
-    );
+    )
   }
-});
+}
 
-module.exports = Anime;
+module.exports = Anime
