0648564fe945dd700dd2a6b09a25ac52db123e86
kae_kasui <kaekasui_github@icloud.com>
2016-11-13 16:35:53 +0900

    Display admin actors table

---
+++ app/assets/javascripts/components/admin/actors/admin_actors_table.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import AdminActorRow from './admin_actor_row.jsx'
+
+export default React.createClass({
+  loadActorsFromServer() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: (res) => {
+        this.setState({actors: res.actors})
+      },
+      error: (xhr, status, err) => {
+        console.error(this.props.url, status, err.toString())
+      }
+    })
+  },
+
+  componentDidMount() {
+    this.loadActorsFromServer()
+  },
+
+  getInitialState() {
+    return { actors: [] }
+  },
+
+  render() {
+    return (
+      <table className='table'>
+        <tbody>
+          {this.state.actors.map((actor) =>
+            <AdminActorRow key={actor.id} actor={actor}>{actor.title}</AdminActorRow>
+          )}
+        </tbody>
+      </table>
+    )
+  }
+})
