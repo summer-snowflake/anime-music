85ffb7bd14f1682a44c71dd0a40a4e7343c02db9
kae_kasui <kaekasui_github@icloud.com>
2016-11-15 00:46:12 +0900

    Add phase column to season table

--- app/models/season.rb
+++ app/models/season.rb
@@ -2,5 +2,5 @@ class Season < ApplicationRecord
   belongs_to :anime
   has_many :melodies
 
-  validates :name, presence: true
+  validates :phase, presence: true
 end
--- spec/factories/seasons.rb
+++ spec/factories/seasons.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :season do
     anime
+    sequence(:phase) { |n| "第#{n}期" }
     sequence(:name) { |n| "シーズン名#{n}" }
     start_on { Time.zone.today - 10.days }
     end_on { [Time.zone.today, nil].sample }
