eb27cb4ebc098409663d5e17c41fc78fdcbc83d5
kae_kasui <kaekasui_github@icloud.com>
2016-11-18 00:42:40 +0900

    Fix test for table columns

--- app/assets/javascripts/components/admin/actors/admin_actor_row.jsx
+++ app/assets/javascripts/components/admin/actors/admin_actor_row.jsx
@@ -5,9 +5,6 @@ class AdminActorRow extends React.Component {
     return (
       <tr>
         <td>{this.props.actor.name}</td>
-        <td><a href={'/admin/actors/' + this.props.actor.id}>Show</a></td>
-        <td><a href={'/admin/actors/' + this.props.actor.id + '/edit'}>Edit</a></td>
-        <td><a href={'/admin/actors/' + this.props.actor.id} data-method='delete'>Destroy</a></td>
       </tr>
     )
   }
--- app/assets/javascripts/components/admin/animes/admin_anime_row.jsx
+++ app/assets/javascripts/components/admin/animes/admin_anime_row.jsx
@@ -9,10 +9,6 @@ export default class AdminAnimeRow extends Component {
         <td>{this.props.anime.summary}</td>
         <td>{this.props.anime.wiki_url}</td>
         <td>{this.props.anime.picture}</td>
-        <td><a href={'/admin/animes/' + this.props.anime.id}>Show</a></td>
-        <td><a href={'/admin/animes/' + this.props.anime.id + '/edit'}>Edit</a></td>
-        <td><a href={'/admin/animes/' + this.props.anime.id} data-method='delete'>Destroy</a></td>
-        <td><Link to='/admin'>Admin link</Link></td>
       </tr>
     )
   }
