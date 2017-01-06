9d9cc3ef5f93abcdae749d1fa7f44b11345e6b49
kae_kasui <kaekasui_github@icloud.com>
2017-01-07 00:15:58 +0900

    Add test to display dom

---
+++ app/assets/javascripts/__tests__/admin/animes/admin_anime_row.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import expect from 'expect'
+import { Link } from 'react-router'
+import { createRenderer } from 'react-addons-test-utils'
+import expectJSX from 'expect-jsx'
+
+expect.extend(expectJSX)
+
+import AdminAnimeRow from '../../../components/admin/animes/admin_anime_row'
+jest.unmock('../../../components/admin/animes/admin_anime_row')
+
+describe('AnimeComponent', () => {
+  let renderer = createRenderer()
+
+  it('propsに設定した値が出力される', () => {
+    const anime = { id: 1, title: 'アニメタイトル', summary: 'アニメサマリ', wiki_url: 'https://wiki.com', picture: '' }
+    renderer.render(
+      <AdminAnimeRow anime={anime} />
+    )
+    let actualElement = renderer.getRenderOutput()
+    let expectedElement = (
+      <tr id='anime-1'>
+        <td><Link to='/admin/animes/1'>{'アニメタイトル'}</Link></td>
+        <td>{'アニメサマリ'}</td>
+        <td>{'https://wiki.com'}</td>
+        <td>{''}</td>
+      </tr>
+    )
+    expect(actualElement).toEqualJSX(expectedElement)
+  })
+})
