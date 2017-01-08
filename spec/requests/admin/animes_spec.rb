09a0b31ee5bd6b903ba1d2f7ed8b9508fbd8cab1
kae_kasui <kaekasui_github@icloud.com>
2017-01-09 06:55:59 +0900

    Return the seasons info of the anime

--- spec/requests/admin/animes_spec.rb
+++ spec/requests/admin/animes_spec.rb
@@ -31,6 +31,8 @@ end
 
 describe 'GET /api/admin/animes/:id', autodoc: true do
   let!(:anime) { create(:anime) }
+  let!(:season1) { create(:season, anime: anime) }
+  let!(:season2) { create(:season, anime: anime) }
 
   it '200とアニメ詳細が返ってくること' do
     get "/api/admin/animes/#{anime.id}"
@@ -41,7 +43,23 @@ describe 'GET /api/admin/animes/:id', autodoc: true do
       title: anime.title,
       summary: anime.summary,
       wiki_url: anime.wiki_url,
-      picture: anime.picture
+      picture: anime.picture,
+      seasons: [
+        {
+          id: season1.id,
+          phase: season1.phase,
+          name: season1.name,
+          start_on: season1.start_on.strftime('%Y-%m-%d'),
+          end_on: season1.end_on.try(:strftime, '%Y-%m-%d')
+        },
+        {
+          id: season2.id,
+          phase: season2.phase,
+          name: season2.name,
+          start_on: season2.start_on.strftime('%Y-%m-%d'),
+          end_on: season2.end_on.try(:strftime, '%Y-%m-%d')
+        }
+      ]
     }
     expect(response.body).to be_json_as(json)
   end
