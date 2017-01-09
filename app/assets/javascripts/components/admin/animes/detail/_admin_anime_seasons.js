070010d217f3f6f732c0794fb5ee71c110f2921a
kae_kasui <kaekasui_github@icloud.com>
2017-01-09 09:14:38 +0900

    Add test for admin anime seasons component

--- app/assets/javascripts/components/admin/animes/detail/_admin_anime_seasons.js
+++ app/assets/javascripts/components/admin/animes/detail/_admin_anime_seasons.js
@@ -2,6 +2,13 @@ import React, { Component, PropTypes } from 'react'
 import AdminAnimeSeason from './_admin_anime_season'
 
 export default class AdminAnimeSeasons extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      seasons: []
+    }
+  }
+
   render() {
     return (
       <div className='adminAnimeSeasonsComponent'>
