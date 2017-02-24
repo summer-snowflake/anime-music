88f0cfd76742af0c3ec36fe811813b3dfe0f6041
kae_kasui <kaekasui_github@icloud.com>
2017-02-24 19:06:46 +0900

    Modify the design for thumbnail of the anime detail page

--- app/assets/javascripts/components/admin/animes/detail/_admin_anime_thumbnail.js
+++ app/assets/javascripts/components/admin/animes/detail/_admin_anime_thumbnail.js
@@ -6,7 +6,11 @@ export default class AdminAnimeThumbnail extends Component {
       <div className='adminAnimeThumbnailComponent'>
         <div className='col-xs-6 col-md-3'>
           <a className='thumbnail' href='#'>
-            <img alt={this.props.title} src={this.props.picture} />
+            {this.props.picture ? (
+              <img alt={this.props.title} src={this.props.picture} />
+            ) : (
+              <span className='no-image'>{'NO IMAGE'}</span>
+            )}
           </a>
         </div>
       </div>
