3d6ec0df7f5af91850953b4d35acd892b09508cc
kae_kasui <kaekasui_github@icloud.com>
2017-02-27 17:49:50 +0900

    Add test to update anime title

--- spec/features/admin/animes/anime_title_spec.rb
+++ spec/features/admin/animes/anime_title_spec.rb
@@ -6,12 +6,10 @@ feature '管理画面：アニメ', js: true do
   let!(:anime2) { create(:anime) }
 
   background do
-    visit admin_animes_path
+    visit admin_anime_path(id: anime1.id)
   end
 
   scenario '詳細画面のタイトルでタイトルを編集できること' do
-    visit admin_anime_path(id: anime1.id)
-
     expect(page).to have_content anime1.title
     find('.not-editing-title > span.link').click
     fill_in 'title', with: 'アニメのタイトル'
@@ -19,6 +17,33 @@ feature '管理画面：アニメ', js: true do
 
     expect(page).not_to have_content anime1.title
     expect(page).to have_content 'アニメのタイトル'
+    expect(find('.alert-success')).to be_visible
+
     expect(anime1.reload.title).to eq 'アニメのタイトル'
+  end
+
+  scenario '詳細画面のタイトルで空のタイトルに変更できないこと' do
+    old_title = anime1.title
+    expect(page).to have_content old_title
+    find('.not-editing-title > span.link').click
+    fill_in 'title', with: ' '
+    find("input[name='title']").native.send_keys(:backspace)
+    expect(find("input[name='title']").value).to eq ''
+    find("input[name='title']").native.send_keys(:return)
+    expect(find('.alert-danger')).to be_visible
+
+    expect(anime1.reload.title).to eq old_title
+  end
+
+  scenario '詳細途中のタイトルのフォーカスを外してからフォーカスすると編集途中に戻ること' do
+    old_title = anime1.title
+    expect(page).to have_content old_title
+
+    find('.not-editing-title > span.link').click
+    fill_in 'title', with: '編集途中のアニメタイトル'
+    find('.adminAnimeTitleComponent').click
+    expect(find('b.panel-title').text).to eq old_title
+    find('.not-editing-title > span.link').click
+    expect(find("input[name='title']").value).to eq '編集途中のアニメタイトル'
   end
 end
