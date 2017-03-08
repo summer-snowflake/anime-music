8a2c22757430882589b0da24aa894f473a29d88e
kae_kasui <kaekasui_github@icloud.com>
2017-03-09 07:48:34 +0900

    Add to update actor name

--- app/assets/javascripts/components/admin/actors/detail/_admin_actor_detail.js
+++ app/assets/javascripts/components/admin/actors/detail/_admin_actor_detail.js
@@ -1,20 +1,28 @@
 import React, { Component, PropTypes } from 'react'
 import { origin } from './../../../../origin.js'
 
+import AdminActorTitle from './_admin_actor_title'
+
 export default class AdminActorDetail extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      actor: { name: '' }
+      actor: { name: '' },
+      editingTitle: false
     }
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   componentDidMount() {
     this.loadActorsFromServer()
   }
 
+  handleSubmit(params) {
+    this.updateActorAgainstServer(params)
+  }
+
   loadActorsFromServer() {
-    fetch(origin + this.props.url)
+    fetch(origin + 'api/admin/actors/' + this.props.actor_id)
       .then((res) => res.json())
       .then((res) => {
         this.setState({actor: res})
@@ -24,14 +32,45 @@ export default class AdminActorDetail extends Component {
       })
   }
 
+  updateActorAgainstServer(params) {
+    fetch(origin + 'api/admin/actors/' + this.props.actor_id, {
+      method: 'PATCH',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params)
+    })
+      .then((res) => {
+        if(res.status == '200') {
+          this.setState({actor: params})
+          if(params.name) {
+            this.refs.name.updateSuccess()
+          }
+        } else {
+          res.json().then((json) => {
+            if(params.name != undefined) {
+              this.refs.name.updateFailed(json.error_messages[0])
+            }
+          })
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   render() {
     return (
       <div className='adminActorDetailComponent'>
         <div className='panel panel-default'>
-          <div className='panel-heading'>
-            {this.state.actor.name}
+          <div className='panel-body'>
+            <AdminActorTitle
+              handleUpdateName={this.handleSubmit}
+              name={this.state.actor.name}
+              ref='name'
+            />
           </div>
-          <div className='panel-body' />
         </div>
       </div>
     )
@@ -39,5 +78,5 @@ export default class AdminActorDetail extends Component {
 }
 
 AdminActorDetail.propTypes = {
-  url: PropTypes.string.isRequired
+  actor_id: PropTypes.string.isRequired
 }
