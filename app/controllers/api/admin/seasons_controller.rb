b5ede0d59676759269c6a1ea08c3c80b6dc6b8a5
kae_kasui <kaekasui_github@icloud.com>
2017-03-09 22:09:35 +0900

    Add the api to add anime season

--- app/controllers/api/admin/seasons_controller.rb
+++ app/controllers/api/admin/seasons_controller.rb
@@ -1,14 +1,27 @@
 # frozen_string_literal: true
 class Api::Admin::SeasonsController < Api::BaseController
-  before_action :set_anime, only: %i(index)
+  before_action :set_anime, only: %i(index create)
 
   def index
     @seasons = @anime.seasons
+  end
+
+  def create
+    @season = @anime.seasons.new(season_params)
+    if @season.save
+      head :created
+    else
+      render_error @season
+    end
   end
 
   private
 
   def set_anime
     @anime = Anime.find(params[:anime_id])
+  end
+
+  def season_params
+    params.require(:season).permit(:name, :start_on, :end_on)
   end
 end
--- spec/requests/admin/seasons_spec.rb
+++ spec/requests/admin/seasons_spec.rb
@@ -31,3 +31,24 @@ describe 'GET /api/admin/animes/1/seasons', autodoc: true do
     expect(response.body).to be_json_as(json)
   end
 end
+
+describe 'POST /api/admin/animes/:anime_id/seasons', autodoc: true do
+  let!(:anime) { create(:anime) }
+  let!(:season_name) { 'シーズンワン' }
+  let!(:three_months_ago) { 3.months.ago.to_date }
+  let!(:one_month_ago) { 1.month.ago.to_date }
+  let!(:params) do
+    { season: { name: season_name, start_on: three_months_ago, end_on: one_month_ago } }
+  end
+
+  context '正しい値を設定した場合' do
+    it '201が返ってくること' do
+      post "/api/admin/animes/#{anime.id}/seasons", params: params
+      expect(response.status).to eq 201
+      season = anime.seasons.last
+      expect(season.name).to eq 'シーズンワン'
+      expect(season.start_on).to eq three_months_ago
+      expect(season.end_on).to eq one_month_ago
+    end
+  end
+end
