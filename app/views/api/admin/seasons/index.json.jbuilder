6683138e4866f54a08330c2968649ee1065590e3
kae_kasui <kaekasui_github@icloud.com>
2017-03-11 01:43:56 +0900

    Add the decorator and Display the period

--- app/views/api/admin/seasons/index.json.jbuilder
+++ app/views/api/admin/seasons/index.json.jbuilder
@@ -5,5 +5,6 @@ json.seasons do
     json.name season.name
     json.start_on season.start_on
     json.end_on season.end_on
+    json.period season.decorate.period
   end
 end
---
+++ spec/decorators/season_decorator_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe SeasonDecorator, type: :decorator do
+  describe '#period' do
+    let!(:start_on) { 3.months.ago.to_date.to_s }
+    let!(:end_on) { 1.month.ago.to_date.to_s }
+    let!(:season) { create(:season, start_on: start_on, end_on: end_on).decorate }
+    subject { season.period }
+
+    context 'start_onとend_onに値が入っている場合' do
+      it { is_expected.to eq start_on + ' 〜 ' + end_on }
+    end
+
+    context 'start_onのみ値が入っている場合' do
+      let(:end_on) { nil }
+
+      it { is_expected.to eq start_on + ' 〜 ' }
+    end
+
+    context 'start_onのみ値が入っている場合' do
+      let(:start_on) { nil }
+
+      it { is_expected.to eq ' 〜 ' + end_on }
+    end
+
+    context 'start_onとend_on両方のデータがない場合' do
+      let(:start_on) { nil }
+      let(:end_on) { nil }
+
+      it { is_expected.to eq '' }
+    end
+  end
+end
