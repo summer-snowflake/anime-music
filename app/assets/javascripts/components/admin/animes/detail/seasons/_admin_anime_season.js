55ed12cebf03ba88bce175ffccee6e08af281249
kae_kasui <kaekasui_github@icloud.com>
2017-03-15 20:53:47 +0900

    Add the edit icon to the each season

--- app/assets/javascripts/components/admin/animes/detail/seasons/_admin_anime_season.js
+++ app/assets/javascripts/components/admin/animes/detail/seasons/_admin_anime_season.js
@@ -4,13 +4,18 @@ export default class AdminAnimeSeason extends Component {
   render() {
     return (
       <div className='adminAnimeSeasonComponent media'>
-        <div className='media-body'>
+        <div className='media-body not-editing-body'>
           <div className='media-heading'>
             {'第' + this.props.season.phase + '期：'}
             {this.props.season.name}
           </div>
           <div className='period'>
             {this.props.season.period}
+          </div>
+          <div className='pull-right'>
+            <span className='link'>
+              <span className='glyphicon glyphicon-pencil' />
+            </span>
           </div>
         </div>
       </div>
