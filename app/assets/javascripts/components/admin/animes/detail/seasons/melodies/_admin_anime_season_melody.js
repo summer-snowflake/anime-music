217359bdb699f076e77c936f731f195ff63ac61a
summer-snow <pig.book.notice@gmail.com>
2017-04-27 22:50:13 +0900

    Display the pencil icon on melody of the season

--- app/assets/javascripts/components/admin/animes/detail/seasons/melodies/_admin_anime_season_melody.js
+++ app/assets/javascripts/components/admin/animes/detail/seasons/melodies/_admin_anime_season_melody.js
@@ -10,7 +10,13 @@ export default class AdminAnimeSeasonMelody extends Component {
       <div className='adminAnimeSeasonMelodyComponent'>
         <div className='label label-info kind-label'>
           {this.props.melody.kind.toUpperCase()}
+          <div className='pull-right'>
+            <span className='link' onClick={this.handleClickTrashIcon}>
+              <span className='glyphicon glyphicon-pencil' />
+            </span>
+          </div>
         </div>
+
         <div className='title'>
           <span className='glyphicon glyphicon-music' />
           {this.props.melody.title}
