a7e8cdd91ec67772af5bd6e0be81d0efa047993f
summer-snow <pig.book.notice@gmail.com>
2017-05-03 17:50:02 +0900

    Remove unnecessary lines

--- app/assets/javascripts/components/admin/animes/detail/advertisements/_admin_anime_advertisement.js
+++ app/assets/javascripts/components/admin/animes/detail/advertisements/_admin_anime_advertisement.js
@@ -6,41 +6,15 @@ export default class AdminAnimeAdvertisement extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      showModal: false,
-      message_type: 'success',
-      message: ''
+      showModal: false
     }
     this.handleClickTrashIcon = this.handleClickTrashIcon.bind(this)
     this.onClickCancelButton = this.onClickCancelButton.bind(this)
     this.onClickDeleteButton = this.onClickDeleteButton.bind(this)
   }
 
-  handleClickEditIcon() {
-    this.setState({showForm: true})
-  }
-
   handleClickTrashIcon() {
     this.setState({showModal: true}) 
-  }
-
-  handleTimeout() {
-    this.setState({message: ''})
-  }
-
-  handleClickSubmitButton() {
-    let params = {
-      season: {
-        phase: this.refs.form.refs.phase.value,
-        name: this.refs.form.refs.name.value,
-        start_on: this.refs.form.refs.start_on.value,
-        end_on: this.refs.form.refs.end_on.value
-      }
-    }
-    this.updateAnimeSeasonAgainstServer(params)
-  }
-
-  handleClickCancelButton() {
-    this.setState({showForm: false})
   }
 
   onClickCancelButton() {
@@ -69,45 +43,6 @@ export default class AdminAnimeAdvertisement extends Component {
       })
   }
 
-  updateAnimeSeasonAgainstServer(params) {
-    fetch(origin + 'api/admin/animes/' + this.props.season.anime_id + '/seasons/' + this.props.season.id, {
-      method: 'PATCH',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Token token=' + localStorage.getItem('access_token')
-      },
-      body: JSON.stringify(params)
-    })
-      .then((res) => {
-        if(res.status == '200') {
-          this.setState({showForm: false, message_type: 'success', message: '更新しました'})
-          setTimeout(this.handleTimeout, 2000)
-          this.loadAnimeSeasonFromServer()
-        } else {
-          res.json().then((json) => {
-            this.refs.form.updateFailed(json.error_messages[0])
-          })
-        }
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }
-
-  loadAnimeSeasonFromServer() {
-    fetch(origin + 'api/admin/animes/' + this.props.season.anime_id + '/seasons/' + this.props.season.id, {
-      headers: {'Authorization': 'Token token=' + localStorage.getItem('access_token')}
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        this.setState({season: res})
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-  }
- 
   render() {
     return (
       <div className='adminAnimeAdvertisementComponent media' id={'advertisement-' + this.props.advertisement.id}>
