468d537666893c7d0889e7026061630a442a8176
summer-snow <pig.book.notice@gmail.com>
2017-05-13 01:36:49 +0900

    Display the melody advertisement on toppage

--- app/assets/javascripts/components/welcome/_movie.js
+++ app/assets/javascripts/components/welcome/_movie.js
@@ -5,6 +5,10 @@ export default class Movie extends Component {
     return (
       <div className='movieComponent' id={'movie-' + this.props.movie.melody_id}>
         <span dangerouslySetInnerHTML={{__html: this.props.movie.youtube}} />
+        <div className='cd pull-right'>
+          <span className='label label-default'>{'CD'}</span>
+          <span dangerouslySetInnerHTML={{__html: this.props.movie.advertisement_body}} />
+        </div>
       </div>
     )
   }
