e2b32703471544b80de281514769a82dc7fa8798
summer-snow <pig.book.notice@gmail.com>
2017-05-18 01:26:25 +0900

    Set the condition of production mode

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,15 @@
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
 
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_REGION']
-  }
-  config.cache_storage = :fog if Rails.env.production?
-  config.fog_public = true
-  config.fog_directory = ENV['AWS_FOG_DIRECTORY']
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION']
+    }
+    config.cache_storage = :fog
+    config.fog_public = true
+    config.fog_directory = ENV['AWS_FOG_DIRECTORY']
+  end
 end
