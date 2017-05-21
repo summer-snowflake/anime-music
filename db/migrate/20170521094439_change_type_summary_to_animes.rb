54a940bc906320bf4b8d3a6721df6602a954d0db
summer-snow <pig.book.notice@gmail.com>
2017-05-21 18:50:06 +0900

    Modify to devide to up and down methods

--- db/migrate/20170521094439_change_type_summary_to_animes.rb
+++ db/migrate/20170521094439_change_type_summary_to_animes.rb
@@ -1,5 +1,9 @@
 class ChangeTypeSummaryToAnimes < ActiveRecord::Migration[5.1]
-  def change
+  def up
     change_column :animes, :summary, :text
+  end
+
+  def down
+    change_column :animes, :summary, :string
   end
 end
