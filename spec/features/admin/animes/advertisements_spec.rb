0a42145492f57982504ba3be89c2218928af5883
summer-snow <pig.book.notice@gmail.com>
2017-05-22 02:34:28 +0900

    Add feature spec

--- spec/features/admin/animes/advertisements_spec.rb
+++ spec/features/admin/animes/advertisements_spec.rb
@@ -20,6 +20,8 @@ feature '管理画面：アニメ詳細：広告', js: true do
 
       within '.adminAnimeAdvertisementsComponent' do
         expect(page).to have_css "a[href='https://url.com']"
+        expect(page).to have_content advertisement1.tag_name
+        expect(page).to have_content advertisement2.tag_name
       end
     end
 
@@ -36,13 +38,16 @@ feature '管理画面：アニメ詳細：広告', js: true do
         find('.btn-danger').click
       end
       expect(page).to have_no_css "#advertisement-#{advertisement1.id}"
+      expect(page).to have_no_content advertisement1.tag_name
       expect(page).to have_css "#advertisement-#{advertisement2.id}"
+      expect(page).to have_content advertisement2.tag_name
     end
   end
 
   scenario 'アニメの広告を登録できること' do
     within '.adminAnimeAdvertisementNewFieldComponent' do
       find('.btn-primary').click
+      fill_in 'tag_name', with: '広告のタグ名'
       fill_in 'body', with: "<a href='https://example.com'>URL</a>"
       find('.btn-danger').click
     end
@@ -50,6 +55,7 @@ feature '管理画面：アニメ詳細：広告', js: true do
     within '.adminAnimeAdvertisementsComponent' do
       expect(page).to have_content 'URL'
       expect(page).to have_css "a[href='https://example.com']"
+      expect(page).to have_content '広告のタグ名'
     end
   end
 end
