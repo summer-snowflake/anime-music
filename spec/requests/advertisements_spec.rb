cc7c1f8d7005aeeaa70edb797fe5e7303ef728a3
summer-snow <pig.book.notice@gmail.com>
2017-05-21 10:00:17 +0900

    Add spec for advertisements list

--- spec/requests/advertisements_spec.rb
+++ spec/requests/advertisements_spec.rb
@@ -9,7 +9,7 @@ describe 'GET /api/seasons/:season_id/advertisements', autodoc: true do
   let!(:advertisement2) { create(:advertisement, anime: anime) }
 
   context 'ログインしていない場合' do
-    it '401が返ってくること' do
+    it '200とデータが返ってくること' do
       get "/api/seasons/#{season.id}/advertisements"
       expect(response.status).to eq 200
 
@@ -25,6 +25,35 @@ describe 'GET /api/seasons/:season_id/advertisements', autodoc: true do
           }
         ]
       }
+      expect(response.body).to be_json_as(json)
+    end
+  end
+end
+
+describe 'GET /api/advertisements', autodoc: true do
+  let!(:anime) { create(:anime) }
+  let!(:season) { create(:season, anime: anime) }
+  let!(:advertisement1) { create(:advertisement, anime: anime) }
+  let!(:advertisement2) { create(:advertisement, anime: anime) }
+
+  context 'ログインしていない場合' do
+    it '200とデータが返ってくること' do
+      get "/api/advertisements"
+      expect(response.status).to eq 200
+
+      json = {
+        advertisements: contain_exactly(
+          {
+            id: advertisement1.id,
+            body: advertisement1.body
+          },
+          {
+            id: advertisement2.id,
+            body: advertisement2.body
+          }
+        )
+      }
+      json = JSON.parse(response.body)
       expect(response.body).to be_json_as(json)
     end
   end
