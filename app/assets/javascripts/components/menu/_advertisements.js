37650b10ba32e81296bcb9ff079426b84f373b1e
summer-snow <pig.book.notice@gmail.com>
2017-05-21 18:57:43 +0900

    Remove the unnecessary lines

--- app/assets/javascripts/components/menu/_advertisements.js
+++ app/assets/javascripts/components/menu/_advertisements.js
@@ -5,22 +5,6 @@ import { origin } from './../../origin.js'
 export default class Advertisements extends Component {
   constructor(props) {
     super(props)
-    this.handleRefreshAdvertisements = this.handleRefreshAdvertisements.bind(this)
-  }
-
-  handleRefreshAdvertisements() {
-    this.loadAdvertisementsFromServer()
-  }
-
-  loadAdvertisementsFromServer() {
-    fetch(origin + 'api/seasons/' + this.props.season_id + '/advertisements')
-      .then((res) => res.json())
-      .then((res) => {
-        this.setState({advertisements: res.advertisements})
-      })
-      .catch((error) => {
-        console.error(error)
-      })
   }
 
   render () {
@@ -38,6 +22,5 @@ export default class Advertisements extends Component {
 }
 
 Advertisements.propTypes = {
-  advertisements: PropTypes.array.isRequired,
-  season_id: PropTypes.number
+  advertisements: PropTypes.array.isRequired
 }
