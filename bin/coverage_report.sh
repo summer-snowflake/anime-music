357898e5f156afd97970132471326d95f2feafcc
summer-snow <pig.book.notice@gmail.com>
2017-05-26 20:41:51 +0900

    Modify the sh file for coverage

--- bin/coverage_report.sh
+++ bin/coverage_report.sh
@@ -1,12 +1,17 @@
-STATUSES_API_URL="https://api.github.com/repos/ClinicalPlatform/Qualizm/statuses/${CIRCLE_SHA1}"
+STATUSES_API_URL="https://api.github.com/repos/summer-snowflake/anime-music/statuses/${CIRCLE_SHA1}"
 
 COVERED_PERCENT=`ruby -rjson -e "puts JSON.parse(File.read('coverage/.last_run.json'))['result']['covered_percent']"`
 
-SIMPLECOV_TARGET_URL="https://$CIRCLE_BUILD_NUM-71275953-gh.circle-artifacts.com/0/$CIRCLE_ARTIFACTS/coverage/index.html"
+SIMPLECOV_TARGET_URL="https://$CIRCLE_BUILD_NUM-71275953-gh.circle-artifacts.com/0/home/ubunts/anime-music/coverage/index.html"
 SIMPLECOV_DESCRIPTION="${COVERED_PERCENT}% covered"
+GITHUB_STATUS="failure"
 
-curl  -H "Authorization: token $GH_STATUS_TOKEN" \
-      -H "Content-Type: application/json" \
-      -H "Accept: application/vnd.github.v3+json" \
-      -d "{\"description\": \"$SIMPLECOV_DESCRIPTION\", \"state\": \"success\", \"context\": \"simplecov\", \"target_url\": \"$SIMPLECOV_TARGET_URL\"}" \
+if ${GITHUB_STATUS} -gt 98 ; then
+  GITHUB_STATUS="success"
+fi
+
+curl -H "Authorization: token $GITHUB_API_TOKEN" \
+     -H "Content-Type: application/json" \
+     -H "Accept: application/vnd.github.v3+json" \
+     -d "{\"description\": \"$SIMPLECOV_DESCRIPTION\", \"state\": \"$GITHUB_STATUS\", \"context\": \"simplecov\", \"target_url\": \"$SIMPLECOV_TARGET_URL\"}" \
       $STATUSES_API_URL
