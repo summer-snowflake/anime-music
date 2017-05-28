84af9e48b8aa246c5540938bbe298ea4604e5890
summer-snow <pig.book.notice@gmail.com>
2017-05-28 23:04:17 +0900

    Rename column from name to behind_name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525171831) do
+ActiveRecord::Schema.define(version: 20170528140226) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,7 @@ ActiveRecord::Schema.define(version: 20170525171831) do
 
   create_table "seasons", id: :serial, force: :cascade do |t|
     t.integer "anime_id", null: false
-    t.string "name"
+    t.string "behind_name"
     t.date "start_on"
     t.date "end_on"
     t.datetime "created_at", null: false
