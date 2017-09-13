503d1096cab401f08d4ddee0aeb1eb5e132770bb
summer-snow <pig.book.notice@gmail.com>
2017-09-13 18:40:26 +0900

    Add status column to animes table for unpublished, prepared and published status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170910082027) do
+ActiveRecord::Schema.define(version: 20170913092757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170910082027) do
     t.datetime "updated_at", null: false
     t.integer "created_by"
     t.integer "updated_by"
+    t.integer "status", default: 0, null: false
   end
 
   create_table "appearances", id: :serial, force: :cascade do |t|
