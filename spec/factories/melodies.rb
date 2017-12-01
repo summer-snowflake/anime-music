01c3feda7bdc4bc0b9fedd6b38f9e7c506d7191c
kaekasui <kaekasui_github@icloud.com>
2017-12-01 13:28:09 +0900

    Modify from factory_girl to factory_bot

--- spec/factories/melodies.rb
+++ spec/factories/melodies.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-FactoryGirl.define do
+FactoryBot.define do
   factory :melody do
     trait :with_anime do
       anime
--- spec/factories/singers.rb
+++ spec/factories/singers.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-FactoryGirl.define do
+FactoryBot.define do
   factory :singer do
     transient do
       gimei { Gimei.new }
